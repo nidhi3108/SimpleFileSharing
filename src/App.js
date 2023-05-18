3c52b9d61c39db92fe0775ca3370a03a5e9142a3
Nidhi Verma <nidhivr310@gmail.com>
2023-05-18 22:31:17 +0530

    SimpleFileSharing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// document.queryslector id and class arereplaced by useref in recat
+import React, { useEffect, useState } from "react";
+import {useRef} from "react";
 import './App.css';
+import download from './assets/download.jpg'
+// import { post } from "../../server/router/utilrouter";
 
 function App() {
+  const [selfile,setSelFile]=useState('');
+  const fileInputRef=useRef();
+
+  const submit = async (formdata) => {
+    // formdata.name=setSelFile;
+    formdata.file = selfile;
+    console.log(formdata);
+
+    const response = await fetch("http://localhost:8000/file/add", {
+      method: 'POST',
+      body: formdata,
+    });
+   console.log(response);
+  };
+
+
+  const onuploadclick=()=>{
+    // .current se dom element aata hai
+    fileInputRef.current.click();
+  }
+  console.log(selfile);
+
+
+ const uploadfile=(e)=>{
+  const file=e.target.files
+     setSelFile(file.name)
+     console.log(setSelFile);
+     const data= new FormData();
+     data.append('myfile',file)
+     fetch("http://localhost:8000/util/uploadfile",{
+      method: "POST",
+      body: data,
+     })
+     .then((res)=>{
+        if(res.status===200)
+        console.log("file uploaded");
+        else
+        console.log("kuch to gadbad h");
+     })
+     .catch((err)=>{
+      console.log("file not uploaded");
+     })
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={download} alt=""/>
+      <div className="wrapper">
+        <h1>Simple File Sharing</h1>
+        <p>Upload and share the download link.</p>
+        <button onClick={onuploadclick}>Upload</button>
+        <input type="file"
+        ref={fileInputRef} 
+        style={{display:"none"}}
+        onChange={uploadfile}
+        // onChange={(e)=> setFile(e.target.files[0])}
+        />
+        <button onClick={submit}>Submit</button>
+      </div>
     </div>
   );
 }
